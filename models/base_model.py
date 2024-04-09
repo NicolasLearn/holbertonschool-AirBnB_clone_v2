8cb6792628145e3d2a7f4d07ba3c9bbda85a4f90
nicolas <nicolas.quoirez@hotmail.fr>
2024-04-09 15:23:52 +0200

    update b_m.py to create instance

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid
+from uuid import uuid4
 from datetime import datetime
 import models
 from sqlalchemy import Column, Integer, String, DateTime
@@ -17,23 +17,19 @@ class BaseModel:
     updated_at = Column(DateTime, nullable=False, default=(datetime.now()))
 
     def __init__(self, *args, **kwrgs):
-        """Instatntiates a new model"""
-        if not kwrgs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            if 'updated_at' in kwrgs:
-                kwrgs['updated_at'] = datetime.strptime(kwrgs['updated_at'],
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
-            if 'created_at' in kwrgs:
-                kwrgs['created_at'] = datetime.strptime(kwrgs['created_at'],
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
-            if '__class__' in kwrgs:
-                del kwrgs['__class__']
-            self.__dict__.update(kwrgs)
+        """Initialize a new BaseModel.
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.now()
+        if kwrgs:
+            for key, value in kwrgs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -46,6 +42,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
