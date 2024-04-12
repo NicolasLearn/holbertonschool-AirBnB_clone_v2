ebb4aec41d751a400b5fe9c1c013036c90aa7cf8
nicolas <nicolas.quoirez@hotmail.fr>
2024-04-12 11:00:05 +0200

    writing script and hml file to task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module starts a web application with Flask.
+
+The application listens on 0.0.0.0, port 5000.
+Teardown:
+    close_session: Remove the current session.
+Routes:
+    /cities_by_states: HTML page with a list of all State objects in DBStorage.
+"""
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of all states and related cities.
+    States/cities are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
